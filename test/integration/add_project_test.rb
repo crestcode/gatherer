3b202cc824f41cc5702966c16b6146762008f783
John Kasiewicz <crestcode@gmail.com>
2014-06-22 15:22:00 -0400

    add project and task resources

---
+++ test/integration/add_project_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class AddProjectTest < Capybara::Rails::TestCase
+  test 'a user can add a project and give it tasks' do
+    visit new_project_path
+    fill_in 'Name', with: 'Project Runway'
+    fill_in 'Tasks', with: 'Task 1:3\nTask 2:5'
+    click_on 'Create Project'
+    visit projects_path
+    assert_content 'Project Runway'
+    assert_content '8'
+  end
+end
