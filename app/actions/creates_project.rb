8ba4d5e33ab51b88068c0a38c1f1d51653b6f81a
John Kasiewicz <crestcode@gmail.com>
2014-06-27 15:22:00 -0400

    build out creates_project model

--- app/actions/creates_project.rb
+++ app/actions/creates_project.rb
@@ -8,5 +8,24 @@ class CreatesProject
 
   def build
     self.project = Project.new(name: name)
+    project.tasks = convert_string_to_tasks
+    project
+  end
+
+  def convert_string_to_tasks
+    task_string.split('\n').map do |task_string|
+      title, size = task_string.split(':')
+      size = 1 if (size.blank? || size.to_i.zero?)
+      Task.new(title: title, size: size)
+    end
+  end
+
+  def save
+    project.save
+  end
+
+  def create
+    build
+    save
   end
 end
